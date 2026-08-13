f14931b282811c2d9cb45b53d268ba223b1fd966
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2026-08-13 11:28:49 -0400

    update checkout endpoint

--- docs/checkout/config.js
+++ docs/checkout/config.js
@@ -1,4 +1,4 @@
 window.FLOPPY_CHECKOUT_CONFIG = Object.freeze({
   // The backend must allow https://floppysoftware.com (without a path) via CORS.
-  checkoutEndpoint: "https://2a4cjoxwud.execute-api.us-east-1.amazonaws.com",
+  checkoutEndpoint: "https://2a4cjoxwud.execute-api.us-east-1.amazonaws.com/v1/checkout",
 });
